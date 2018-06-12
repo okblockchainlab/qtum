58b1db397472f9d5e2c633f85b062825dcf52fd2
zhongqiuwood <zhongqiuwood@gmail.com>
2018-06-12 22:07:09 +0800

    upd

--- runbuild.sh
+++ runbuild.sh
@@ -2,14 +2,26 @@
 
 git submodule init && git submodule update
 
-
 ./autogen.sh
+
 ./configure --with-gui=no --enable-debug -disable-gui-tests --disable-bench
-#make -j 6 V=1
+
+EXT=so
+
+TARGET_OS=`uname -s`
+
+case "$TARGET_OS" in
+    Darwin)
+        EXT=dylib
+        ;;
+    Linux)
+        EXT=so
+        ;;
+    *)
+        echo "Unknown platform!" >&2
+        exit 1
+esac
 
 cd src
-make qtumd.so -f Makefile.qtumd.so -j 6 V=1
-nm qtumd.so |grep okcoin
-
-make qtumd.dylib -f Makefile.qtumd.so -j 6 V=1
-nm qtumd.dylib |grep okcoin
+make qtumd.${EXT} -f Makefile.qtumd.so -j 6 V=1
+nm qtumd.${EXT} |grep okcoin
